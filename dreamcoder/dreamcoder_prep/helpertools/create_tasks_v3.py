c8c9b5c96a06a94298fb0cec80c75864d67683b9
Kevin <kevin.kampa@stud.tu-darmstadt.de>
2025-03-28 07:48:33 +0100

    Added scaling to bboxes during transform

--- dreamcoder/dreamcoder_prep/helpertools/create_tasks_v3.py
+++ dreamcoder/dreamcoder_prep/helpertools/create_tasks_v3.py
@@ -18,16 +18,18 @@ def create_annotations_json(input_file_positive, input_file_negative, output_fol
     os.makedirs(false_folder, exist_ok=True)
 
     # Function to convert bbox format and create output annotations
-    def process_annotations(data, folder):
+    def process_annotations(data, folder, maxdim=None):
         for image in data['images']:
             image_id = image['id']
+            imagemax = max(image['height'], image['width'])
+            factor = maxdim/imagemax if type(maxdim) is int else 1
             annotations_for_image = [
                 {
                     'boxes': [
-                        annotation['bbox'][0],  # x
-                        annotation['bbox'][1],  # y
-                        annotation['bbox'][0] + annotation['bbox'][2],  # xmax
-                        annotation['bbox'][1] + annotation['bbox'][3]   # ymax
+                        annotation['bbox'][0] if type(maxdim) is not int else round(annotation['bbox'][0] * factor),  # xmin
+                        annotation['bbox'][1] if type(maxdim) is not int else round(annotation['bbox'][1] * factor),  # ymin
+                        annotation['bbox'][0] + annotation['bbox'][2] if type(maxdim) is not int else round((annotation['bbox'][0] + annotation['bbox'][2]) * factor),  # xmax
+                        annotation['bbox'][1] + annotation['bbox'][3] if type(maxdim) is not int else round((annotation['bbox'][1] + annotation['bbox'][3]) * factor)  # ymax
                     ],
                     'labels': annotation['category_id']
                 }
@@ -42,17 +44,25 @@ def create_annotations_json(input_file_positive, input_file_negative, output_fol
                     json.dump({'annotations': annotations_for_image}, out_f, indent=4)
 
     # Process annotations for positive examples
-    process_annotations(data_positive, true_folder)
+    process_annotations(data_positive, true_folder, 500)
 
     # Process annotations for negative examples
-    process_annotations(data_negative, false_folder)
+    process_annotations(data_negative, false_folder, 500)
 
 if __name__ == "__main__":
     # Example usage
     input_file = 'annotations_filtered_by_traffic_sign_right-of_vehicle'  # Replace with your actual input file
+    if len(sys.argv) > 1 and not sys.argv[1].isdigit(): #grab input filename
+        input_file = sys.argv[1]
+        args = sys.argv[2:]
+    else:
+        args = sys.argv[1:]
     input_file_positive = os.path.splitext(input_file)[0] + '_positive.json'
     input_file_negative = os.path.splitext(input_file)[0] + '_negative.json'
     output_folder_name = os.path.splitext(os.path.basename(input_file))[0]
     
     create_annotations_json(input_file_positive, input_file_negative, output_folder_name)
 
+
+    
+    
